8c1692c761719e56e46e44f50fb7b09c52006640
MagoraMaksym <maksym.mahora@lnu.edu.ua>
2026-05-04 21:58:12 +0300

    Update main.cpp
    Refactored main function to create and start vehicles using polymorphism.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <windows.h>
 #include "vehicle.h"
 
 int main() {
-    SetConsoleOutputCP(65001);
-    std::cout << "\n--- Об'єкт Car ---";
-    Car* my_car = new Car();
-    my_car->CreateVehicle();
-    my_car->StartEngine();
-    delete my_car;
 
-    std::cout << "\n\n--- Об'єкт ElectricCar ---";
-    ElectricCar* my_ev = new ElectricCar();
-    my_ev->CreateVehicle();
-    my_ev->StartEngine();
-    delete my_ev;
+    std::cout << "\n1. Створюємо Car:";
+    Vehicle* v1 = new Car();
+    v1->StartEngine();
+    delete v1; 
 
-    std::cout << "\n\n--- Об'єкт Truck ---";
-    Truck* my_truck = new Truck();
-    my_truck->CreateVehicle();
-    my_truck->StartEngine();
-    delete my_truck;
+    std::cout << "\n\n2. Створюємо ElectricCar:";
+    Vehicle* v2 = new ElectricCar();
+    v2->StartEngine();
+    delete v2; 
+    
+
+    std::cout << "\n\n3. Створюємо Truck:";
+    Vehicle* v3 = new Truck();
+    v3->StartEngine();
+    delete v3; 
+
 
     return 0;
 }
